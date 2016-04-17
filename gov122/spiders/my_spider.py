96f0d980a61ab346c6fd5cb6d2959feed4b8653a
qukaile <qukaile@qq.com>
2016-04-17 23:34:43 +0800

    增加车型item

--- gov122/spiders/my_spider.py
+++ gov122/spiders/my_spider.py
@@ -53,16 +53,18 @@ class GovSpider(scrapy.spiders.Spider):
         for each in sites.get("data"):
             item = Gov122Item()
             kcmc = each.get("kcmc")		# 考场名称："三门峡市XXX考场"
+            kscx = each.get("kscx")                 # 考试车型：C1/C2/B2
             kskm = each.get("kskm")             # 考试科目
             kkrs = each.get("kkrs")		# 考试计划人数
             kscc = each.get("kscc")		# 考试场次：上午/下午
             ksrq = each.get("ksrq")		# 考试日期：格式2016-05-20
-            # 打印：三门峡市XXX考场----KeMu1----2016-04-11(上午)----400;
-            print "%s----KeMu%s----%s(%s)----%s"%(kcmc,kskm,ksrq,kscc,kkrs)
+            # 打印：三门峡市XXX考场C1----KeMu1----2016-04-11(上午)----400;
+            print "%s%s----KeMu%s----%s(%s)----%s"%(kcmc,kscx,kskm,ksrq,kscc,kkrs)
 
             # 若要收集Item，将爬取结果保存到数据库或进行其他操作。增加以下代码，并参照Scrapy文档拓展功能
             #item["kcmc"] = each["kcmc"]        # 考场名称："三门峡市XXX考场"
             #item["kskm"] = each["kskm"]        # 考试科目
+            #item["kscx"] = each["kscx"]        # 考试车型：C1/C2/B2
             #item["kkrs"] = each["kkrs"]        # 考试计划人数
             #item["kscc"] = each["kscc"]        # 考试场次：上午/下午
             #item["ksrq"] = each["ksrq"]        # 考试日期：格式2016-05-20

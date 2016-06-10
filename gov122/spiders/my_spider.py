3ec73b68cd330f74a81fc5be0df32f394b17b708
qukaile <qukaile@qq.com>
2016-06-11 01:10:24 +0800

    增加了科目四，重新排列输出格式

--- gov122/spiders/my_spider.py
+++ gov122/spiders/my_spider.py
@@ -10,6 +10,15 @@ class GovSpider(scrapy.spiders.Spider):
     name = "gov122"             # scrapy crawl gov122来启动爬虫
     allowed_domains = ["ha.122.gov.cn"]
     start_urls = ["http://ha.122.gov.cn/m/examplan/getExamPlanDetail"]
+    headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip,deflate",
+    "Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
+    "Connection": "keep-alive",
+    "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+    "Referer": "http://ha.122.gov.cn/views/examplanpub.html"
+    }
     
 
     def parse(self, response):
@@ -30,7 +39,8 @@ class GovSpider(scrapy.spiders.Spider):
 
         # 列举了以下考场：1.三门峡考练考场科目一；2.渑池县车管所科目一考场；3.义马科目一考场；4.三门峡市交警支队科目二考场(隐藏)；
         # 5.三门峡科目二交通考场；6.三门峡科目二渑池车管所考场；7.三门峡渑池友谊考场；8.三门峡支队科目三小车考场； 9.三门峡渑池小车科目三考场；
-        #10.三门峡科目二全顺考场C2；11.三门峡市交警支队科目四考场；12.三门峡科目二全顺考场B2；13.三门峡B2科目三；14.三门峡C2科目三。
+        #10.三门峡科目二全顺考场C2；11.三门峡市交警支队科目四考场；12.三门峡科目二全顺考场B2；13.三门峡B2科目三；14.三门峡C2科目三;
+        #15.渑池县车管所科目四考场; 16.三门峡支队科目四义马考场
         # 下面2行增加查询三门峡考练考场科目一
         kao_lian_1 = {'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '4112101', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '0'}
         add_formdata(kao_lian_1)
@@ -73,6 +83,13 @@ class GovSpider(scrapy.spiders.Spider):
         # 三门峡C2科目三
         kao_lian_c2_3 = {'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '4112302', 'kscx': 'C2','startTime': start_time, 'endTime': '2116-06-03','zt': '0'}
         add_formdata(kao_lian_c2_3)
+        # 渑池县车管所科目四考场
+        mian_chi_4 = {'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '4112181', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '0'}
+        add_formdata(mian_chi_4)
+        # 三门峡支队科目四义马考场
+        yi_ma_4 = {'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '4112131', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '0'}
+        add_formdata(yi_ma_4)
+
 
         # 遍历所有考场
         for each in smx_formdata:
@@ -90,7 +107,13 @@ class GovSpider(scrapy.spiders.Spider):
             item["kkrs"] = each["kkrs"]        # 考试计划人数
             item["kscc"] = each["kscc"]        # 考试场次：上午/下午
             item["ksrq"] = each["ksrq"]        # 考试日期：格式2016-05-20
+            print "|%s|%s|%s| %s"%(format((u'\u79d1\u76ee'+each["kskm"]), '^5'), format((each["ksrq"]+'('+each["kscc"])+')', '^16'), 
+                                    format((each["kkrs"]+u'\u4eba'), '^10'), (each["kcmc"]+each["kscx"]))
+            
+
+
+
             # 打印：三门峡市XXX考场C1----科目1----2016-04-11(上午)----400人
-            print "%s%s----%s%s----%s(%s)----%s%s"%(each["kcmc"],each["kscx"],u'\u79d1\u76ee',each["kskm"],each["ksrq"],each["kscc"],each["kkrs"],u'\u4eba')
+            #print "%s%s----%s%s----%s(%s)----%s%s"%(each["kcmc"],each["kscx"],u'\u79d1\u76ee',each["kskm"],each["ksrq"],each["kscc"],each["kkrs"],u'\u4eba')
             items.append(item)
         return items
--- gov122/spiders/result_spider.py
+++ gov122/spiders/result_spider.py
@@ -10,6 +10,15 @@ class ResultSpider(scrapy.spiders.Spider):
     name = "gov122_result"             # scrapy crawl gov122_result 来启动爬虫
     allowed_domains = ["ha.122.gov.cn"]
     start_urls = ["http://ha.122.gov.cn/m/examplan/getExamPlanResult"]
+    headers = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip,deflate",
+    "Accept-Language": "en-US,en;q=0.8,zh-TW;q=0.6,zh;q=0.4",
+    "Connection": "keep-alive",
+    "Content-Type":" application/x-www-form-urlencoded; charset=UTF-8",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36",
+    "Referer": "http://ha.122.gov.cn/views/examplanpub.html"
+    }
     
 
     def parse(self, response):
@@ -29,11 +38,23 @@ class ResultSpider(scrapy.spiders.Spider):
                 return smx_result_formdata
 
         # 科目一
-        result_formdata_1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
-        add_result_formdata(result_formdata_1)
+        #result_formdata_1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        #add_result_formdata(result_formdata_1)
+        result_formdata_1_b = {'page': '1', 'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_1_b)
+        result_formdata_1_c = {'page': '2', 'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_1_c)
+        result_formdata_1_d = {'page': '3', 'fzjg': '%E8%B1%ABM', 'kskm': '1','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_1_d)
         # 科目二C1
-        result_formdata_2_c1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
-        add_result_formdata(result_formdata_2_c1)
+        #result_formdata_2_c1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        #add_result_formdata(result_formdata_2_c1)
+        result_formdata_2_c1_b = {'page': '1', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_2_c1_b)
+        result_formdata_2_c1_c = {'page': '2', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_2_c1_c)
+        result_formdata_2_c1_d = {'page': '3', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_2_c1_d)
         # 科目二b2
         result_formdata_2_b2 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '2','ksdd': '', 'kscx': 'B2','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
         add_result_formdata(result_formdata_2_b2)
@@ -44,14 +65,26 @@ class ResultSpider(scrapy.spiders.Spider):
         result_formdata_3_b2 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'B2','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
         add_result_formdata(result_formdata_3_b2)
         # 科目三c1
-        result_formdata_3_c1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
-        add_result_formdata(result_formdata_3_c1)
+        #result_formdata_3_c1 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        #add_result_formdata(result_formdata_3_c1)
+        result_formdata_3_c1_b = {'page': '1', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_3_c1_b)
+        result_formdata_3_c1_c = {'page': '2', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_3_c1_c)
+        result_formdata_3_c1_d = {'page': '3', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_3_c1_d)
         # 科目三c2
         result_formdata_3_c2 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '3','ksdd': '', 'kscx': 'C2','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
         add_result_formdata(result_formdata_3_c2)
         # 科目四
-        result_formdata_4 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
-        add_result_formdata(result_formdata_4)
+        #result_formdata_4 = {'page': '0', 'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        #add_result_formdata(result_formdata_4)
+        result_formdata_4_b = {'page': '1', 'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_4_b)
+        result_formdata_4_c = {'page': '2', 'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_4_c)
+        result_formdata_4_d = {'page': '3', 'fzjg': '%E8%B1%ABM', 'kskm': '4','ksdd': '', 'kscx': 'C1','startTime': start_time, 'endTime': '2116-06-03','zt': '2'}
+        add_result_formdata(result_formdata_4_d)
 
         # 遍历所有
         for each in smx_result_formdata:
@@ -76,8 +109,9 @@ class ResultSpider(scrapy.spiders.Spider):
             # 打印需要的考场公布信息{4112101:三门峡市交警支队科目四考场, 4112181:渑池县车管所科目一考场, 4112131:义马科目一考场, 4112208:三门峡科目二交通考场, 4112211:三门峡科目二渑池车管所考场, 411210:三门峡渑池友谊考场, 4112302:三门峡支队科目三小车考场, 4112304:三门峡渑池小车科目三考场, 4112207:三门峡科目二全顺考场B2C2}
             the_list = ['4112101', '4112181', '4112131', '4112208', '4112211', '411210', '4112302', '4112304', '4112207']
             if each["ksdd"] in the_list:
+                print "|%s|%s|%s|%s| %s"%(format((u'\u79d1\u76ee'+each["kskm"]), '^5'), format((each["ksrq"]+'('+each["kscc"])+')', '^16'), 
+                                    format((each["ztStr"]+u'\u4eba'), '^10'), format(u'\u5df2\u516c\u5e03', '^4'), (each["kcmc"]+each["kscx"]))
                 # 打印：三门峡市XXX考场C1----科目1----2016-04-11(上午)----395/400人----已公布
-                print "%s%s----%s%s----%s(%s)----%s%s----%s"%(each["kcmc"],each["kscx"],u'\u79d1\u76ee',each["kskm"],each["ksrq"],each["kscc"],each["ztStr"],u'\u4eba',u'\u5df2\u516c\u5e03')
-
+                #print "%s%s----%s%s----%s(%s)----%s%s----%s"%(each["kcmc"],each["kscx"],u'\u79d1\u76ee',each["kskm"],each["ksrq"],each["kscc"],each["ztStr"],u'\u4eba',u'\u5df2\u516c\u5e03')
             items.append(item)
         return items
